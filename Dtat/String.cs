29027f79c7ff7d520836dff643709b5e0cae2336
Mr. Dariush Tasdighi <DariushT@GMail.com>
2023-07-16 16:59:12 +0330

    Version 1.0.6 - Clean Code

--- Dtat/String.cs
+++ Dtat/String.cs
@@ -6,6 +6,26 @@ public static class String : object
 	{
 	}
 
+	//public static string? Fix(string? text)
+	//{
+	//	if (string.IsNullOrWhiteSpace(value: text))
+	//	{
+	//		return null;
+	//	}
+
+	//	text =
+	//		text.Trim();
+
+	//	while (text.Contains("  "))
+	//	{
+	//		text = text.Replace
+	//			(oldValue: "  ", newValue: " ");
+	//	}
+
+	//	return text;
+	//}
+
+
 	public static string? Fix(this string? text)
 	{
 		if (string.IsNullOrWhiteSpace(value: text))
